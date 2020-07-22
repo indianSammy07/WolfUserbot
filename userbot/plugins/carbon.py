2abab2145c94f6f2b4c8631ff8b248fe139285a0
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-22 16:51:55 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [FRIDAY](https://www.github.com/midhunkm1294-bit/FRIDAY)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [WOLFUSERBOT](https://github.com/indianSammy07/Wolfuserbot)>> ",
 
          force_document=True,
 
@@ -160,7 +160,7 @@ async def carbon_api(e):
 
          )
 
-   os.remove('./FRIDAY.png')
+   os.remove('./wolf.png')
 
    driver.quit()
 
