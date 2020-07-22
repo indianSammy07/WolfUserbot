f2dcbb78505bfb2cf95dfe37009a62588783b10c
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-22 14:49:41 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -2,7 +2,7 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 print ("")
 print ("")
-print(""" To Friday String Generator By StarkGang""")
+print(""" To Wolf String Generator By WolfGang""")
 print("""Kindly Enter Your Details To Continue ! """)
 
 API_KEY = '1754367'
@@ -12,7 +12,7 @@ while True:
    with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
       print("")
       session = client.session.save()
-      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @FridayOT For Any Help !")
+      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @WolfUserbot_Support For Any Help !")
       print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
       print("Store it safe !!")
   except:
