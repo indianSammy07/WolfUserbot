b5507d1257566e2138ab8cda3d1152b6905b29a3
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-22 18:07:38 +0530

    Update information.py

--- userbot/plugins/information.py
+++ userbot/plugins/information.py
@@ -44,7 +44,7 @@ async def _(event):
     except Exception as e:
         dc_id = "`Need a Profile Picture to check **this**`"
         location = str(e)
-    caption = """<b>Extracted Userdata From Telegram DATABASE By Friday<b>
+    caption = """<b>Extracted Userdata From Telegram DATABASE By WOLFUSERBOT<b>
 <b>🔥Telegram ID</b>: <code>{}</code>
 <b>🤟Permanent Link</b>: <a href='tg://user?id={}'>Click Here</a>
 <b>🗣️First Name</b>: <code>{}</code>
