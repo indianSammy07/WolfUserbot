4e144d9a20c2c129e613aefc9f5ddfba945d4589
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-22 18:26:15 +0530

    Update upload_dl.py

--- userbot/plugins/upload_dl.py
+++ userbot/plugins/upload_dl.py
@@ -1,5 +1,5 @@
-# © By StarkGang™ And IndianBot™
-# For F.r.i.d.a.y And Indianbot ™
+# © By WolfGang™ And MaDNooB™
+# For Wolf And Indianbot ™
 """ Userbot module which contains everything related to \
     downloading/uploading from/to the server. """
 
