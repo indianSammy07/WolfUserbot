69f955c0adb99309b3b1a968e40ecc5fa37d512f
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-22 18:43:50 +0530

    Update slap.py

--- userbot/plugins/slap.py
+++ userbot/plugins/slap.py
@@ -73,7 +73,7 @@ HIT = [
     "bashes",
 ]
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "FridayUserbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "WolfUserbot"
 
 @borg.on(admin_cmd(pattern="slap ?(.*)", allow_sudo=True))
 async def who(event):
