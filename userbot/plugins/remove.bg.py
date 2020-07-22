ab744341040376b66ceae9ea7312e63d67785a34
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-22 18:50:18 +0530

    Update remove.bg.py

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -25,7 +25,7 @@ async def _(event):
         message_id = event.reply_to_msg_id
         reply_message = await event.get_reply_message()
         # check if media message
-        await event.edit("Connecting to official Friday server and analysing that img ...")
+        await event.edit("Connecting to official Wolf server and analysing that img ...")
         try:
             downloaded_file_name = await borg.download_media(
                 reply_message,
@@ -47,7 +47,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "@FridayOT.png"
+            remove_bg_image.name = "@wolf.png"
             await borg.send_file(
                 event.chat_id,
                 remove_bg_image,
@@ -58,9 +58,9 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by Friday Userbot".format(ms))
+        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by Wolf Userbot".format(ms))
     else:
-        await event.edit("ReMove.BG API returned Errors. Please report to @FridayOt Support Group\n`{}".format(output_file_name.content.decode("UTF-8")))
+        await event.edit("ReMove.BG API returned Errors. Please report to @wolfuserbotOt Support Group\n`{}".format(output_file_name.content.decode("UTF-8")))
 
 
 # this method will call the API, and return in the appropriate format
