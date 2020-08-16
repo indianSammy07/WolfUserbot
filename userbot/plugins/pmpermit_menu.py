64f05af760848c2ea20cde9386b922eae1098ba1
onlyforuserbot <66107206+onlyforuserbot@users.noreply.github.com>
2020-08-16 22:27:37 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -39,8 +39,8 @@ async def _(event):
                "`4`. To request something\n")
          ONE = ("`I Have Registered Your Request ! Don't Worry My Master Will Be Here Soon To Chat With You !` \n\n")
          TWO = ("`Please Don't Spam My Master Inbox ! You Have Been Reported Until Further Notice !`")
-         FOUR = ("`Okay ! I See You Can Request Your Demands ! Please Wait Untill My Master Approves You` !")
-         FIVE = ("`Okay. please have the basic manners as to not bother my master too much. If he wishes to help you, he will respond to you soon.`\n**Kindly Do not ask repeatdly else you will be blocked and reported.**")
+         THREE = ("`Okay ! I See You Can Request Your Demands ! Please Wait Untill My Master Approves You` !")
+         FOUR = ("`Okay. please have the basic manners as to not bother my master too much. If he wishes to help you, he will respond to you soon.`\n**Kindly Do not ask repeatdly else you will be blocked and reported.**")
          LWARN = ("**This is your last warning. DO NOT send another message else you will be blocked and reported. Keep patience. My master will respond you ASAP.**\n__Use__ `/start` __to go back to the main menu.__")
      
         async with borg.conversation(chat) as conv:
@@ -73,7 +73,7 @@ async def _(event):
          
 
          elif y == "3":
-             await borg.send_message(chat, FOUR)
+             await borg.send_message(chat, THREE)
              response = await conv.get_response(chat)
              await event.delete()
              await response.delete()
@@ -86,7 +86,7 @@ async def _(event):
                      await asyncio.sleep(3)
                      await event.client(functions.contacts.BlockRequest(chat_id))
          elif y == "4":
-             await borg.send_message(chat,FIVE)
+             await borg.send_message(chat,FOUR)
              response = await conv.get_response(chat)
              if not response.text == "/start":
                  await borg.send_message(chat, LWARN)
@@ -106,4 +106,3 @@ async def _(event):
                      await borg.send_message(chat, TWO)
                      await asyncio.sleep(3)
                      await event.client(functions.contacts.BlockRequest(chat_id))
-
